53a435ed847d221d9d4befb706df28add5cf765f
Kyle Wang <kyle@Kyles-MacBook-Pro.local>
2019-12-09 23:02:12 -0800

    set up gin template

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
